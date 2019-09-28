0e998096e508ad1595c05c986584dd7513feee8d
gustavo <gustavopraxedes@live.com>
2019-09-27 21:38:06 -0300

    projeto

---
+++ projetoAtletas/src/main/java/com/cotemig/projetoAtletas/ProjetoAtletasApplication.java
@@ -0,0 +1,13 @@
+package com.cotemig.projetoAtletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAtletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAtletasApplication.class, args);
+	}
+
+}
